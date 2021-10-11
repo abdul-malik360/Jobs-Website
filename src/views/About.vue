901cf3142f4d17bd81f957e28e288c3ede2fc891
Abdul-Malik <82204978+abdul-malik360@users.noreply.github.com>
2021-10-11 16:08:31 +0200

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nihil
+      harum perspiciatis, quod ex, reprehenderit sequi quidem quos culpa sed
+      nostrum delectus explicabo, numquam tempore maxime debitis laborum? Rerum,
+      magni non. Dolores possimus laboriosam molestiae modi maxime architecto
+      quis corrupti numquam odit quae, ad hic excepturi ab delectus?
+      Consequatur, maiores quidem autem voluptatum in eveniet delectus et.
+      Dolore, quaerat, doloremque molestias, autem molestiae nobis sequi rem
+      rerum libero totam aspernatur culpa deserunt a! Ipsum explicabo
+      consequuntur omnis rem odit sapiente quam corrupti voluptate dolore! Sunt
+      officia dolore voluptas magni illo provident quam asperiores labore,
+      distinctio nostrum a eligendi. Quam, ducimus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nihil
+      harum perspiciatis, quod ex, reprehenderit sequi quidem quos culpa sed
+      nostrum delectus explicabo, numquam tempore maxime debitis laborum? Rerum,
+      magni non. Dolores possimus laboriosam molestiae modi maxime architecto
+      quis corrupti numquam odit quae, ad hic excepturi ab delectus?
+      Consequatur, maiores quidem autem voluptatum in eveniet delectus et.
+      Dolore, quaerat, doloremque molestias, autem molestiae nobis sequi rem
+      rerum libero totam aspernatur culpa deserunt a! Ipsum explicabo
+      consequuntur omnis rem odit sapiente quam corrupti voluptate dolore! Sunt
+      officia dolore voluptas magni illo provident quam asperiores labore,
+      distinctio nostrum a eligendi. Quam, ducimus.
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+      asperiores, aliquid repellendus, aspernatur dicta cum dolor voluptatum quo
+      delectus aliquam, aperiam reprehenderit rerum debitis fuga alias tempora
+      totam. Eligendi, quas eveniet. Repellendus impedit blanditiis velit fugit
+      voluptatibus alias sunt nobis porro asperiores labore aperiam repudiandae
+      saepe ratione, ex non aliquid doloribus delectus amet ipsum nesciunt
+      ipsam? Ut reiciendis numquam fuga maxime tempore maiores deleniti
+      molestias culpa veritatis cupiditate! Dolorem deserunt, itaque fuga dolore
+      recusandae perspiciatis consequatur debitis esse quaerat aliquam libero
+      cupiditate placeat laboriosam iste optio praesentium enim eius. Esse
+      adipisci voluptates repellendus in officia reiciendis porro itaque earum
+      praesentium.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+      asperiores, aliquid repellendus, aspernatur dicta cum dolor voluptatum quo
+      delectus aliquam, aperiam reprehenderit rerum debitis fuga alias tempora
+      totam. Eligendi, quas eveniet. Repellendus impedit blanditiis velit fugit
+      voluptatibus alias sunt nobis porro asperiores labore aperiam repudiandae
+      saepe ratione, ex non aliquid doloribus delectus amet ipsum nesciunt
+      ipsam? Ut reiciendis numquam fuga maxime tempore maiores deleniti
+      molestias culpa veritatis cupiditate! Dolorem deserunt, itaque fuga dolore
+      recusandae perspiciatis consequatur debitis esse quaerat aliquam libero
+      cupiditate placeat laboriosam iste optio praesentium enim eius. Esse
+      adipisci voluptates repellendus in officia reiciendis porro itaque earum
+      praesentium.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
